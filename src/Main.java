70419ed75114a96c9892994741a270e29021534e
Usachev13 <2893318@gmail.com>
2023-10-22 23:03:10 +0300

    Done hw

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
+import java.util.*;
+
 public class Main {
+    private static final List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    private static final List<String> strings = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task4();
     }
+
+    public static void task1() {
+        for (Integer num : nums) {
+            if (num % 2 != 0) {
+                System.out.println(num);
+            }
+        }
+    }
+
+    public static void task2() {
+        Collections.sort(nums);
+
+        int lastNum = Integer.MIN_VALUE;
+        for (Integer integer : nums) {
+            if (integer % 2 == 0 && integer != lastNum) {
+                System.out.println(integer);
+                lastNum = integer;
+            }
+        }
+    }
+
+    public static void task3() {
+        Set<String> uniqueWords = new HashSet<>(strings);
+        System.out.println(uniqueWords);
+    }
+
+    public static void task4() {
+        Set<String> uniqueWords = new HashSet<>(strings);
+        int total = 0;
+        for (String string : uniqueWords) {
+            for (String s : strings) {
+                if (Objects.equals(string, s)) {
+                    total += 1;
+                }
+            }
+
+        }
+        System.out.println(strings.size() - uniqueWords.size());
+        System.out.println(total);
+
+    }
+
+
 }
